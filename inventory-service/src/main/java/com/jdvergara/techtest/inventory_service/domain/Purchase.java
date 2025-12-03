aeb8c772bfd9d337aaaaca101d1408f6783344a9
José Daniel Vergara Lozano <j.d.vergara@cgiar.org>
2025-12-03 18:13:31 -0500

    Purchase model created

--- inventory-service/src/main/java/com/jdvergara/techtest/inventory_service/domain/Purchase.java
+++ inventory-service/src/main/java/com/jdvergara/techtest/inventory_service/domain/Purchase.java
@@ -2,16 +2,18 @@ package com.jdvergara.techtest.inventory_service.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Entity
-@Table(name = "inventory_items")
+@Table(name = "purchases")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class InventoryItem {
+public class Purchase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,9 +25,12 @@ public class InventoryItem {
     @Column(nullable = false)
     private Integer quantity;
 
-    @Column(name = "created_at", updatable = false, insertable = false)
-    private OffsetDateTime createdAt;
+    @Column(name = "unit_price", nullable = false)
+    private BigDecimal unitPrice;
 
-    @Column(name = "updated_at", insertable = false)
-    private OffsetDateTime updatedAt;
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(name = "purchased_at", insertable = false)
+    private OffsetDateTime purchasedAt;
 }
